3ff740c78dadeb648a80186a9bf3c508bab44ba6
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-05-15 14:40:34 +0900

    first commit

---
+++ src/main/java/easyweb/easywebeureka/EasyWebEurekaApplication.java
@@ -0,0 +1,13 @@
+package easyweb.easywebeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyWebEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyWebEurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/easyweb/easywebeureka/EasyWebEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package easyweb.easywebeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyWebEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
