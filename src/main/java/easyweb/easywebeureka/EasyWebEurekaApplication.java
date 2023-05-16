d2b1faea3fef9d822a716fb6a0e29542778e8f19
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-05-16 10:28:19 +0900

    [ADD] application setting

--- src/main/java/easyweb/easywebeureka/EasyWebEurekaApplication.java
+++ src/main/java/easyweb/easywebeureka/EasyWebEurekaApplication.java
@@ -2,8 +2,10 @@ package easyweb.easywebeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EasyWebEurekaApplication {
 
     public static void main(String[] args) {
