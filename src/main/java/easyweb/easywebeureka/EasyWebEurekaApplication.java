03025e0a9a0baa313f406dd3e0bfdb5aaa53ed23
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-05-18 10:02:11 +0900

    [ADD] Http URI 로그 필터 등록

--- src/main/java/easyweb/easywebeureka/EasyWebEurekaApplication.java
+++ src/main/java/easyweb/easywebeureka/EasyWebEurekaApplication.java
@@ -1,8 +1,11 @@
 package easyweb.easywebeureka;
 
+import easyweb.easywebeureka.component.RequestLoggingFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -10,6 +13,14 @@ public class EasyWebEurekaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EasyWebEurekaApplication.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean<RequestLoggingFilter> loggingFilter() {
+        FilterRegistrationBean<RequestLoggingFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new RequestLoggingFilter());
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
     }
 
 }
